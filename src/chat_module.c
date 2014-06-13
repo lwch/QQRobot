de2816fdbd11fd0540ecb60789d3c2c6f73532a4
lwch <lwch748@gmail.com>
2014-06-13 20:40:12 +0800

    修正空内容会崩溃的BUG

--- src/chat_module.c
+++ src/chat_module.c
@@ -144,6 +144,7 @@ static int is_study_msg(msg_content_array_t* content, msg_content_array_t* left,
                 size_t array_count = str_split(content->vals[i].string.ptr, "=>", &array);
                 size_t j = 0;
 
+                if (array_count == 0) continue;
                 if (str_empty(array[j])) ++j;
                 if (mode == APPEND_LEFT)
                 {
